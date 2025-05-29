ef4579f4dd2e8b23ecf52aa5f59e2aacb727ef93
Bongithemba <radebebongithemba@gmail.com>
2025-05-29 13:03:03 +0200

    added photo gallery

---
+++ script.js
@@ -0,0 +1,19 @@
+//Photo albu that is displayed on the landing page/
+var slideIndex = 1;
+showIndex(slideIndex);
+
+function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n){
+    var i;
+    var x = document.getElementsByClassName("row");
+    if (n > x.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = x.length};
+    for (i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+//*******************end*********************/
